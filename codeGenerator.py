6dd89129a0f93af7221c2e984267d093f9b1a7a6
Cassard Mathieu <cassard.mathieu@gmail.com>
2014-05-04 17:53:38 +0200

    Python generated code is almost good (not sur it work, but does not explode)
    Signed-off-by: Cassard Mathieu <cassard.mathieu@gmail.com>

--- codeGenerator.py
+++ codeGenerator.py
@@ -1,11 +1,12 @@
+import sys
 import re
 
 # Attribute object
 class Attribute(object):
-	def __init__(self, type, name, octet=0):
+	def __init__(self, type, name, size=0):
 		self.type = type
 		self.name = name
-		self.octet = octet
+		self.size = size
 
 class Function(object):
 	def __init__(self, name="", type="", attributes=[]):
@@ -14,82 +15,110 @@ class Function(object):
 		self.type = type
 		self.attributes = attributes
 
+	def getAttributesLen(self):
+		res=0
+		for attribute in self.attributes :
+			res+=int(attribute.size)
+		return res
 
-# Read a file
-file = open("input.py", "r")
-input = file.readlines()
 
-functions = []
-new_function = None
+def parseFile(filename):
+	# Read a file
+	file = open(filename, "r")
+	input = file.readlines()
 
-for line in input:
-	# Parse tags
-	if re.match(r"^#\ @(\w+)", line):
-		words=line.split();
-		tag=words[1]
+	print("Parsing "+filename)
 
-		if tag == "@method":
-			if new_function!=None :
-				functions.append(new_function)
-				new_function = None
-			new_function=Function()
-			new_function.name = words[2]
-
-		elif tag == "@type":
-			new_function.type = words[2]
-
-		elif tag == "@param":
-			type = words[2]
-			name = words[-1]
-			octet = words[-2]
-			new_function.attributes.append(Attribute(type, name, octet))
-
-if new_function!=None :
-	functions.append(new_function)
+	functions = []
 	new_function = None
 
+	for line in input:
+		# Parse tags
+		if re.match(r"^#\ @(\w+)", line):
+			words=line.split();
+			tag=words[1]
 
-# Write a file
-output = "i2c_registers = {\n"
+			if tag == "@method":
+				if new_function!=None :
+					functions.append(new_function)
+					new_function = None
+				new_function=Function()
+				new_function.attributes=[] #fix a bug :/
+				new_function.name = words[2]
 
-# I2C Register
-i = 1
-for function in functions:
-	output += "	\"REG_" + function.name.upper() + "\":" + str(i) + ",\n"
-	i += 1
+			elif tag == "@type":
+				new_function.type = words[2]
 
-output += "}\n\n"
+			elif tag == "@param":
+				type = words[2]
+				name = words[-1]
+				size = int(words[-2])
+				new_function.attributes.append(Attribute(type, name, size))
 
+	if new_function!=None :
+		functions.append(new_function)
+		new_function = None
 
-# Functions definition
-for function in functions:
-	output += "def i2c_" + function.name + "("
-	if function.type == "setter":
-		for attribute in function.attributes:
-			output += attribute.name + ","
+	return functions
 
-		output = output[:-1] # Remove the last character
-		output += "):\n	vals=[]\n"
+def generatePython(filename,functions):
 
-		for attribute in function.attributes:
-			output += "	vals.extend(split_" + attribute.type.replace(" ", "_") + "(" + attribute.name + "))\n"
+	print("Generating python code "+filename)
 
-		output += "	bus.write_block_data(address,i2c_registers['REG_" + function.name.upper() +"'],vals)\n\n"
+	# Write a file
+	output = ""
 
-	elif function.type == "getter":
-		output += "):\n"
-		output += "	vals=bus.read_i2c_block_data(address,i2c_registers['REG_" + function.name.upper() + "']," + str( int(function.octet) ) + ")\n"
-		output += "	res=0\n"
-		octet = 0
-		for attribute in function.attributes:
-			output += "	" +attribute.name + "=make_" + attribute.type.replace(" ", "_") + "(vals," + str( int(octet) ) + ")\n"
-			octet += attribute.octet
-		output += "	return "
-		for attribute in function.attributes:
-			output += attribute.name + ","
-		output = output[:-1] # Remove the last character
+	output+="from utils import *\n\n"
+	output+="class GeneratedProxy_i2c():\n\n"
+
+	# I2C Register
+	output += "i2c_registers = {\n"
+	i = 1
+	for function in functions:
+		output += "	\"REG_" + function.name.upper() + "\":" + str(i) + ",\n"
+		i += 1
+
+	output += "}\n\n"
 
 
-#  Write a file
-file = open("output.py", "w")
-file.write(output)
+	# Functions definition
+	for function in functions:
+		output += "def " + function.name + "(self,"
+		if function.type == "setter":
+			for attribute in function.attributes:
+				output += attribute.name + ","
+
+			output = output[:-1] # Remove the last character
+			output += "):\n	vals=[]\n"
+
+			for attribute in function.attributes:
+				output += "	vals.extend(split_" + attribute.type + "_"+ str(attribute.size) + "(" + attribute.name + "))\n"
+
+			output += "	self.bus.write_block_data(address,i2c_registers['REG_" + function.name.upper() +"'],vals)\n\n"
+
+		elif function.type == "getter":
+			output += "):\n"
+			output += "	vals=self.bus.read_i2c_block_data(address,i2c_registers['REG_" + function.name.upper() + "']," + str( int(function.getAttributesLen()) ) + ")\n"
+			output += "	res=0\n"
+			offset = 0
+			for attribute in function.attributes:
+				output += "	" +attribute.name + "=make_" + attribute.type.replace(" ", "_") + "(vals," + str(offset) + ")\n"
+				offset += attribute.size
+			output += "	return "
+			for attribute in function.attributes:
+				output += attribute.name + ","
+			output = output[:-1] # Remove the last character
+			output +="\n\n"
+
+	#little hack to reindent the file
+	output=output.replace("\n", "\n\t")	
+	output=output.replace("\tclass", "class")	
+
+	#  Write a file
+	file = open(filename, "w")
+	file.write(output)
+	file.close
+
+
+functions = parseFile(sys.argv[1])
+generatePython(sys.argv[2],functions)
--- input.py
+++ input.py
@@ -1,12 +1,12 @@
 # @method move
 # @type setter
 # @param integer 32 dist
-# @param bool forward
+# @param bool 8 forward
 
 # @method rotate
 # @type setter
 # @param integer 32 angle
-# @param bool isAbs
+# @param bool 8 isAbs
 
 # @method goto
 # @type setter
@@ -20,46 +20,46 @@
 # @param integer 32 y
 # @param integer 32 angle
 
-# @method setOdo
-# @type setter
-# @param integer 32 x
-# @param integer 32 y
-# @param integer 32 angle
-# @param integer 8 flag
-
-# @method setDistKpKd
-# @type setter
-# @param integer 32 kp
-# @param integer 32 kd
-
-# @method getLinKpKd
-# @type getter
-# @param integer 32 kp
-# @param integer 32 kd
-
-# @method setRotKpKd
-# @type setter
-# @param integer 32 kp
-# @param integer 32 kd
-
-# @method getRotKpKd
-# @type getter
-# @param integer 32 kp
-# @param integer 32 kd
-
-# @method getUltrasounds
-# @type getter
-# @param integer 8 kp
-# @param integer 32 dist
-
-# @method getJumper
-# @type getter
-# @param bool dist
-
-# @method getStatus
-# @type getter
-# @param bool bfr
-# @param bool bfl
-# @param bool bbr
-# @param bool bbl
-# @param bool cmdhack
+-# @method setOdo
+-# @type setter
+-# @param integer 32 x
+-# @param integer 32 y
+-# @param integer 32 angle
+-# @param integer 8 flag
+-
+-# @method setDistKpKd
+-# @type setter
+-# @param integer 32 kp
+-# @param integer 32 kd
+-
+-# @method getLinKpKd
+-# @type getter
+-# @param integer 32 kp
+-# @param integer 32 kd
+-
+-# @method setRotKpKd
+-# @type setter
+-# @param integer 32 kp
+-# @param integer 32 kd
+-
+-# @method getRotKpKd
+-# @type getter
+-# @param integer 32 kp
+-# @param integer 32 kd
+-
+-# @method getUltrasounds
+-# @type getter
+-# @param integer 8 kp
+-# @param integer 32 dist
+-
+-# @method getJumper
+-# @type getter
+-# @param bool dist
+-
+-# @method getStatus
+-# @type getter
+-# @param bool bfr
+-# @param bool bfl
+-# @param bool bbr
+-# @param bool bbl
+-# @param bool cmdhack
